908eac8a54dcd32d31ef15ff540139f936f0c922
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-11-01 16:15:31 +0100

    Removed imports and add <p> for example

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <p>hello</p>
     <App />
   </React.StrictMode>,
-)
+);
