d82862df6ccb4942b5ad9eeeaad98fb191d241ac
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-02-01 14:55:22 +0100

    Create multipurpose for button starting with img's

--- src/components/SaveEdit.jsx
+++ src/components/SaveEdit.jsx
@@ -1,11 +1,32 @@
 import '../styles/SaveEdit.css';
+import { useState } from 'react';
 
 export default function SaveEdit() {
+  const [enable, setEnable] = useState(false);
+  const [imageName, setImageName] = useState('edit-outline');
+
+  const toggleEdit = () => {
+    if (!enable) {
+      setEnable(true);
+      setImageName('save');
+    } else {
+      setEnable(false);
+      setImageName('edit-outline');
+    }
+  };
   return (
     <div className="btn save-edit-btn">
       <button type="button">
-        <img src="/img/edit-outline.svg" alt="edit" className="btn-icon" />
+        <img
+          src={`/img/${imageName}.svg`}
+          alt="save/edit toggle"
+          className="btn-icon"
+          onClick={toggleEdit}
+        />
       </button>
+      {/* <button type="button">
+        <img src="/img/edit-outline.svg" alt="edit" className="btn-icon" />
+      </button> */}
     </div>
   );
 }
