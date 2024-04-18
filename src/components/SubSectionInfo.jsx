53e2e61bed5ac59eb22628e0eeceb531f617a55b
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-04-18 13:13:29 +0200

    Add hide/show functionality if editing or not

--- src/components/SubSectionInfo.jsx
+++ src/components/SubSectionInfo.jsx
@@ -1,25 +1,48 @@
 import '../styles/SubSectionInfo.css';
 import { useState } from 'react';
 
-export default function SubSectionInfo({ what, where }) {
+export default function SubSectionInfo({ what, where, disabled }) {
+  const [dateFrom, setDateFrom] = useState('');
+  const [dateTo, setDateTo] = useState('');
+
+  const handleDateFromChange = (e) => {
+    setDateFrom(e.target.value);
+  };
+
+  const handleDateToChange = (e) => {
+    setDateTo(e.target.value);
+  };
+
   return (
     <div className="sub-section-info">
-      <input type="text" placeholder={what} />
-      <input type="text" placeholder={where} />
+      <input
+        type="text"
+        placeholder={disabled === true ? '' : what}
+        disabled={disabled}
+      />
+      <input
+        type="text"
+        placeholder={disabled === true ? '' : where}
+        disabled={disabled}
+      />
       <div className="from-and-to">
         <input
           type="text"
+          disabled={disabled}
           name="date-from"
           id="date-from"
-          placeholder="mm.yyyy"
+          placeholder={disabled === true ? '' : 'mm.yyyy'}
+          onChange={handleDateFromChange}
           required
         />
-        <p>-</p>
+        <p>{disabled === true && dateFrom === '' ? '' : '-'}</p>
         <input
           type="text"
+          disabled={disabled}
           name="date-to"
           id="date-to"
-          placeholder="mm.yyyy"
+          placeholder={disabled === true && dateTo === '' ? '' : 'mm.yyyy'}
+          onChange={handleDateToChange}
           required
         />
       </div>
