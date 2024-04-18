1e43c5c4ad35f32a766577cf5168dafe8d76282c
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-04-18 13:20:35 +0200

    Add 'hide' props to add hide class name to element

--- src/components/DeleteSection.jsx
+++ src/components/DeleteSection.jsx
@@ -1,13 +1,13 @@
 import '../styles/DeleteSection.css';
 
-export default function DeleteSection({ deleteSub }) {
+export default function DeleteSection({ deleteSub, hide }) {
   return (
     <div className="btn delete-btn">
       <button className="delete-section" onClick={deleteSub} type="button">
         <img
           src="/img/delete.svg"
           alt="delete"
-          className="btn-icon delete-icon"
+          className={`btn-icon delete-icon${hide ? ' hide' : ''}`}
         />
       </button>
     </div>
