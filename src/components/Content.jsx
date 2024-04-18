75ab1693b656a40cf28ac0f964c9963d3ed62995
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-04-18 13:22:32 +0200

    Remove SaveEdit comp. from main component class

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,4 @@
 import '../styles/Content.css';
-import SaveEdit from './SaveEdit';
 import SectionEducation from './SectionEducation';
 import SectionProfile from './SectionProfile';
 import SectionWork from './SectionWork';
@@ -7,11 +6,8 @@ import SectionWork from './SectionWork';
 export default function Content() {
   return (
     <div className="main-container">
-      <SaveEdit />
       <SectionProfile />
-      <SaveEdit />
       <SectionWork />
-      <SaveEdit />
       <SectionEducation />
     </div>
   );
