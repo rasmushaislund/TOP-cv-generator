634ba13b5b25fec81d23169cdfd1dcf6859f947a
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-04-18 13:17:38 +0200

    fix delete feature and add disabling of inputs

--- src/components/SectionEducation.jsx
+++ src/components/SectionEducation.jsx
@@ -3,11 +3,13 @@ import SectionSep from './SectionSep';
 import SubSectionInfo from './SubSectionInfo';
 import AddSubSection from './AddSubSection';
 import DeleteSection from './DeleteSection';
+import SaveEdit from './SaveEdit';
 import { useState } from 'react';
 import { v4 as uuid4 } from 'uuid';
 
 export default function SectionEducation() {
   const [list, setList] = useState([]);
+  const [bool, setBool] = useState(true);
 
   const handleAddSubSection = () => {
     const newSubSection = {
@@ -16,27 +18,49 @@ export default function SectionEducation() {
 
     setList([...list, newSubSection]);
   };
+
+  const handleDeleteSubSection = (id) => {
+    const newList = list.filter((subSection) => subSection.id !== id);
+    setList(newList);
+  };
+
+  const handleToggle = () => {
+    if (!bool) {
+      setBool(true);
+    } else {
+      setBool(false);
+    }
+  };
+
   return (
     <div className="section section-education">
+      <SaveEdit toggle={handleToggle} />
       <SectionSep name={'EDUCATION'} imgPath={'/img/school.svg'} />
       <div className="section-input">
         <ul>
           {list.map((subSection) => (
             <li key={subSection.id}>
-              <SubSectionInfo what={'Degree'} where={'School'} />
+              <SubSectionInfo
+                what={'Degree'}
+                where={'School'}
+                disabled={bool}
+              />
               <textarea
                 name="education-text"
                 id="education-text"
                 rows="5"
-                disabled={false}
+                disabled={bool}
                 maxLength={300}
               ></textarea>
-              <DeleteSection />
+              <DeleteSection
+                deleteSub={() => handleDeleteSubSection(subSection.id)}
+                hide={bool}
+              />
             </li>
           ))}
         </ul>
       </div>
-      <AddSubSection add={handleAddSubSection} />
+      <AddSubSection add={handleAddSubSection} hide={bool} />
     </div>
   );
 }
