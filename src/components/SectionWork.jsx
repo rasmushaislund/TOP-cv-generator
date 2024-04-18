6888b5530288e29b214ec54d66b672aff520a583
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-04-18 13:15:06 +0200

    Fix delete feature and add disabling to inputs

--- src/components/SectionWork.jsx
+++ src/components/SectionWork.jsx
@@ -3,11 +3,13 @@ import AddSubSection from './AddSubSection';
 import SectionSep from './SectionSep';
 import SubSectionInfo from './SubSectionInfo';
 import DeleteSection from './DeleteSection';
+import SaveEdit from './SaveEdit';
 import { useState } from 'react';
 import { v4 as uuid4 } from 'uuid';
 
 export default function SectionWork() {
   const [list, setList] = useState([]);
+  const [bool, setBool] = useState(true);
 
   const handleAddSubSection = () => {
     const newSubSection = {
@@ -22,29 +24,43 @@ export default function SectionWork() {
     setList(newList);
   };
 
+  const handleToggle = () => {
+    if (!bool) {
+      setBool(true);
+    } else {
+      setBool(false);
+    }
+  };
+
   return (
     <div className="section section-work">
+      <SaveEdit toggle={handleToggle} />
       <SectionSep name={'EXPERIENCE'} imgPath={'/img/briefcase.svg'} />
       <div className="section-input">
         <ul>
           {list.map((subSection) => (
             <li key={subSection.id}>
-              <SubSectionInfo what={'Title'} where={'Company'} />
+              <SubSectionInfo
+                what={'Title'}
+                where={'Company'}
+                disabled={bool}
+              />
               <textarea
                 name="work-text"
                 id="work-text"
                 rows="5"
-                disabled={false}
+                disabled={bool}
                 maxLength={300}
               ></textarea>
               <DeleteSection
-                deleteSub={handleDeleteSubSection(subSection.id)}
+                deleteSub={() => handleDeleteSubSection(subSection.id)}
+                hide={bool}
               />
             </li>
           ))}
         </ul>
       </div>
-      <AddSubSection add={handleAddSubSection} />
+      <AddSubSection add={handleAddSubSection} hide={bool} />
     </div>
   );
 }
