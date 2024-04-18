8552c29d5c3111c32e0bd25315479d7db6b20bed
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-04-18 13:16:46 +0200

    Enable disabling of textarea

--- src/components/SectionProfile.jsx
+++ src/components/SectionProfile.jsx
@@ -1,15 +1,28 @@
 import '../styles/SectionProfile.css';
 import SectionSep from './SectionSep';
+import SaveEdit from './SaveEdit';
+import { useState } from 'react';
 
 export default function SectionProfile() {
+  const [bool, setBool] = useState(true);
+
+  const handleToggle = () => {
+    if (!bool) {
+      setBool(true);
+    } else {
+      setBool(false);
+    }
+  };
+
   return (
     <div className="section section-profile">
+      <SaveEdit toggle={handleToggle} />
       <SectionSep name={'PROFILE'} imgPath={'/img/face-man.svg'} />
       <textarea
         name="profile-text"
         id="profile-text"
         rows="8"
-        disabled={false}
+        disabled={bool}
         maxLength={250}
       ></textarea>
     </div>
