a41c643e186bcb2f9e7ed2fc5b46c638487ba0a8
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-05-06 15:17:24 +0200

    Add pdf download functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import { useState } from 'react';
 import './styles/App.css';
-import Download from './components/Download';
+import { Download } from './components/Download';
 import CV from './components/CV';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import { useRef } from 'react';
 
 export default function App() {
+  const pdfRef = useRef();
+
+  const downloadPDF = () => {
+    const input = pdfRef.current;
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4', true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+      const imgX = (pdfWidth - imgWidth * ratio) / 2;
+      const imgY = 0;
+      pdf.addImage(
+        imgData,
+        'PNG',
+        imgX,
+        imgY,
+        imgWidth * ratio,
+        imgHeight * ratio,
+      );
+      pdf.save('cv.pdf');
+    });
+  };
+
   return (
     <div className="app">
-      <Download />
-      <CV />
+      <div className="action-tools">
+        <Download getPDF={downloadPDF} />
+      </div>
+      <div ref={pdfRef}>
+        <CV />
+      </div>
     </div>
   );
 }
