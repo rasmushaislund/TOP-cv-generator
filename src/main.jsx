5709e53c8287daf5f712a6f04d290017d7c88df9
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-05-06 15:17:56 +0200

    Add Headline component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GithubLink from './components/GithubLink.jsx';
+import Headline from './components/Headline.jsx';
 import App from './App.jsx';
 import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GithubLink />
+    <Headline />
     <App />
   </React.StrictMode>,
 );
